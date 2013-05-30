816a1041486963a6ea4220396ed0b4d85b9f3feb
Kel Guerin <futureneer@gmail.com>
2013-05-30 12:19:22 -0400

    Fixed python example application

--- modulair_app_python_example/scripts/python_example_app.py
+++ modulair_app_python_example/scripts/python_example_app.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('modulair_app_python_example')
-import rospy
+import rospy, sys
+# Modulair Core Functionality
+import modulair_core
+from modulair_core import ModulairAppWidget
+# PySide Imports
+import PySide
+from PySide.QtGui import QApplication
+
+class PythonAppWidget(ModulairAppWidget):
+  def __init__(self,name,app):
+    super(PythonAppWidget,self).__init__(name,app)
+    # Put stuff in here and make sure to make any widgets children of 'self'
+    pass
 
 class PythonExampleApp():
   def __init__(self):
-
-    rospy.init_node('modulair_app_python_example',anonymous=True)
+    rospy.init_node('python_example_app',anonymous=True)
+    app = QApplication(sys.argv)
+    app_widget = PythonAppWidget("PythonExampleApp",app)
+    # Running
+    rospy.logwarn("PythonExampleApp: Started")  
+    app.exec_()
+    # Done
     rospy.logwarn('PythonExampleApp: Running')
-    rospy.spin()
     rospy.logwarn('PythonExampleApp: Finished')
 
 # MAIN
